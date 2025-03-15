0f9e552115cbfed42c27ac7aa43d9ad94545f6aa
yuk228 <joyful.nachos-0w@icloud.com>
2025-03-16 02:37:00 +0900

    Refactor

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,6 @@ import Link from "next/link";
 import Typing from "@/components/ui/typing";
 import GlowText from "@/components/glow-text";
 
-
 export default function Home() {
   return (
     <main className="relative min-h-screen">
@@ -33,32 +32,32 @@ export default function Home() {
           </div>
           <div className="py-8 flex flex-col items-center">
             <Typing>I&apos;m Japanese Student</Typing>
-            <Typing>I&apos;m interested in programming and privacy protection</Typing>
+            <Typing>I&apos;m interested in privacy protection</Typing>
             <div className="flex space-x-4 py-5">
               <Image src="/py.webp" alt="py-logo" width={50} height={50}/>
               <Image src="/ts.webp" alt="ts-logo" width={50} height={50}/>
             </div>
             <div className="flex space-x-2 mt-4">
-            <TooltipProvider>
-              <Tooltip>
-                <TooltipTrigger><Link href="https://x.com/yuk228_" className="hover:scale-105"><Twitter size={35}/></Link></TooltipTrigger>
-                <TooltipContent>
-                  <p>Twitter</p>
-                </TooltipContent>
-              </Tooltip>
-              <Tooltip>
-                <TooltipTrigger><Link href="https://github.com/yuk228" className="hover:scale-105"><Github size={35}/></Link></TooltipTrigger>
-                <TooltipContent>
-                  <p>GitHub</p>
-                </TooltipContent>
-              </Tooltip>
-              <Tooltip>
-                <TooltipTrigger><Link href="https://privshare-beta.vercel.app" className="hover:scale-105"><Earth size={35}/></Link></TooltipTrigger>
-                <TooltipContent>
-                  <p>My Blog</p>
-                </TooltipContent>
-              </Tooltip>
-            </TooltipProvider>
+              <TooltipProvider>
+                <Tooltip>
+                  <TooltipTrigger><Link href="https://x.com/yuk228_" className="hover:scale-105"><Twitter size={35}/></Link></TooltipTrigger>
+                  <TooltipContent>
+                    <p>Twitter</p>
+                  </TooltipContent>
+                </Tooltip>
+                <Tooltip>
+                  <TooltipTrigger><Link href="https://github.com/yuk228" className="hover:scale-105"><Github size={35}/></Link></TooltipTrigger>
+                  <TooltipContent>
+                    <p>GitHub</p>
+                  </TooltipContent>
+                </Tooltip>
+                <Tooltip>
+                  <TooltipTrigger><Link href="https://privshare-beta.vercel.app" className="hover:scale-105"><Earth size={35}/></Link></TooltipTrigger>
+                  <TooltipContent>
+                    <p>My Blog</p>
+                  </TooltipContent>
+                </Tooltip>
+              </TooltipProvider>
             </div>
           </div>
         </div>
