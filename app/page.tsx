d2014a00d91b975d7af332984808eaca44735aad
yuk228 <joyful.nachos-0w@icloud.com>
2025-09-25 22:15:00 +0900

    oioioio

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,22 @@
-import Image from "next/image";
-import Typing from "@/components/ui/typing";
-import Card from "@/components/card";
+import { motion } from "motion/react"
 import { Earth, Github, Rss, Twitter } from "lucide-react";
 
 import Link from "next/link";
 
-const cards = [
+const CARDS = [
   {
-    title: "Github",
-    description: "@yuk228",
     icon: <Github />,
-    link: "https://snowing.moe/social/github",
+    link: "social/github",
   },
   {
-    title: "X",
-    description: "@yy28__",
     icon: <Twitter />,
-    link: "https://snowing.moe/social/x",
+    link: "social/x",
   },
   {
-    title: "Zenn.dev",
-    description: "@64919",
     icon: <Rss />,
-    link: "https://snowing.moe/social/zenn",
+    link: "/social/zenn",
   },
   {
-    title: "My Blog",
-    description: "PrivShare",
     icon: <Earth />,
     link: "https://blog.snowing.moe",
   },
@@ -35,53 +25,15 @@ const cards = [
 export default function Page() {
   return (
     <main>
-      <div className="flex items-center justify-center min-h-screen">
-        <div className="container m-4 w-[780px] h-[800px] rounded-3xl flex flex-col items-center shadow-2xl shadow-foreground/10 border-2 border-zinc-500/20">
-          <div className="w-full h-[125px]"></div>
-          <div className="flex items-center justify-center ">
-            <Image
-              src="/icon.webp"
-              alt="icon"
-              width={150}
-              height={150}
-              className="rounded-full border-2 border-white/30 shadow-lg"
-            />
-          </div>
-          <div className="text-center pt-5">
-            <h2 className="text-4xl font-bold">Yuki</h2>
-          </div>
-          <div className="py-8 flex flex-col items-center font-bold">
-            <Typing>日本の学生です</Typing>
-            <Typing>Web系を勉強しています</Typing>
-          </div>
-          <div className="flex flex-col items-center w-full gap-4 px-4">
-            {cards.map((card, index) => (
-              <Card key={index} {...card} />
+      <div className="min-h-screen flex flex-col items-center justify-center max-w-4xl mx-auto">
+        <h2 className="text-4xl font-bold text-center">Hello, I'm Yuki 👋</h2>
+        <div className="pt-10">
+          <div className="flex flex-row gap-8">
+            {CARDS.map((card, index) => (
+              <Link href={card.link} key={index} className="scale-123">
+                {card.icon}
+              </Link>
             ))}
-          </div>
-          <div className="pt-10 pb-2 flex flex-col items-center justify-center text-sm text-muted-foreground">
-            <p>
-              Made with{" "}
-              <span>
-                <Link
-                  href="https://21st.dev"
-                  className="hover:text-foreground transition-colors underline"
-                >
-                  21st.dev
-                </Link>
-              </span>
-            </p>
-            <p>
-              Source code are available on{" "}
-              <span>
-                <Link
-                  href="https://github.com/yuk228/bio"
-                  className="hover:text-foreground transition-colors underline"
-                >
-                  github
-                </Link>
-              </span>
-            </p>
           </div>
         </div>
       </div>
--- app/social/[name]/page.tsx
+++ app/social/[name]/page.tsx
@@ -10,14 +10,10 @@ export default async function Page({ params }: Props) {
   const { name } = await params;
 
   switch (name) {
-    case "signal":
-      return redirect(
-        "https://signal.me/#eu/1wjl2Vi2MfXDUJThkI59fxgCgY_5SidFTuTEYpe82H_ylnjyYz5HSIEHLPeya8FZ",
-      );
     case "x":
       return redirect("https://x.com/yy28__");
     case "zenn":
-      return redirect("https://zenn.dev/yuk228");
+      return redirect("https://zenn.dev/64919");
     case "github":
       return redirect("https://github.com/yuk228");
     default:
