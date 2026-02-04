3d47969eb3d23f0caa24e4dd4b73ee069ec14830
yuk228 <joyful.nachos-0w@icloud.com>
2026-02-05 01:00:46 +0900

    feat: add session maker

---
+++ backend/database/__init__.py
@@ -0,0 +1,20 @@
+from sqlalchemy.engine.create import create_engine
+from sqlalchemy.engine.url import URL
+from sqlalchemy.orm import sessionmaker
+
+from settings import MYSQL_DATABASE, MYSQL_HOST_NAME, MYSQL_PASSWORD, MYSQL_USER
+
+url = URL(
+    drivername="musql+mysqldb",
+    username=MYSQL_USER,
+    password=MYSQL_PASSWORD,
+    host=MYSQL_HOST_NAME,
+    database=MYSQL_DATABASE,
+    query={"charset": "utf8"},
+)
+
+engine = create_engine(url, pool_recycle=10)
+
+
+def create_session():
+    return sessionmaker(bind=engine)()
--- backend/settings.py
+++ backend/settings.py
@@ -6,3 +6,11 @@ load_dotenv()
 
 
 DEBUG = os.getenv("DEBUG", False)
+TIME_ZONE = os.getenv("TZ", "Asia/Tokyo")
+
+MYSQL_ROOT_PASSWORD = os.getenv("MYSQL_ROOT_PASSWORD", "")
+MYSQL_DATABASE = os.getenv("MYSQL_DATABASE", "")
+MYSQL_USER = os.getenv("MYSQL_USER", "")
+MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD", "")
+MYSQL_PORT = int(os.getenv("MYSQL_PORT", "3306"))
+MYSQL_HOST_NAME = os.getenv("MYSQL_HOST_NAME", "")
