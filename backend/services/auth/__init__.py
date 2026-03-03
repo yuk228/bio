72a18a2d38702a6edc0f16bd11b236bd33bdc729
yuk228 <joyful.nachos-0w@icloud.com>
2026-03-03 22:52:31 +0900

    a

--- backend/services/auth/__init__.py
+++ backend/services/auth/__init__.py
@@ -1,10 +1,12 @@
 from sqlalchemy.orm import Session
 from entities.auth.email import EmailLoginDto
+from services.users import find_user_by_email
 
 
 def login_with_email(
     dto: EmailLoginDto,
     session: Session
 ):
-    # user = find_user_by_email()
+    user = find_user_by_email(dto.email, session)
+
     pass
